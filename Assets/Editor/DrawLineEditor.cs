300b901993bd3438a5d556d01cec49b6f83c0949
Haoyu Li <lihaoyu2678@gmail.com>
2017-12-14 16:38:39 -0800

    first commit

---
+++ Assets/Editor/DrawLineEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof (DrawLine))]
+public class DrawLineEditor : Editor {
+
+    private DrawLine drawline;
+	
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        drawline = target as DrawLine;
+
+        if (GUILayout.Button("Draw!"))
+        {
+            drawline.Draw(drawline.start,drawline.end);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine: MonoBehaviour {
+
+    public Vector3 start;
+    public Vector3 end;
+
+    public GameObject line;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+
+    public void Draw (Vector3 startPos, Vector3 endPos)
+    {
+        GameObject go = Instantiate(line,this.transform) as GameObject;
+        //go.transform.localPosition = new Vector3((startPos.x+endPos.x)/2,90f,(startPos.z+endPos.z)/2);
+        go.transform.localPosition = new Vector3(startPos.x,90f,startPos.z);
+        Quaternion localRot = go.transform.localRotation;
+        localRot.eulerAngles = new Vector3(90f,0f,Mathf.Atan2(endPos.z-startPos.z,endPos.x-startPos.x) * Mathf.Rad2Deg);
+        go.transform.transform.localRotation = localRot;
+        Projector p = go.GetComponent<Projector>();
+        float f = Mathf.Sqrt((endPos.z-startPos.z)*(endPos.z-startPos.z)+(endPos.x-startPos.x)*(endPos.x-startPos.x))/50f;
+        StartCoroutine(DrawAnimation(f,p));
+    }
+
+    IEnumerator DrawAnimation(float des, Projector p)
+    {
+        p.aspectRatio = 0.1f;
+        while (p.aspectRatio < des)
+        {
+            p.aspectRatio += 0.25f;
+            yield return null;
+        }
+    }
+
+}
